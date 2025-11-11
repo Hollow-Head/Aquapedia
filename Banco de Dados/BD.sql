f52f0f4769f86b1dbc523b4d6e8341b416d95e3b
Aloísio M. Lingo Filho <60991374+laosioor@users.noreply.github.com>
2025-11-11 16:32:36 -0300

    added database files

---
+++ Banco de Dados/BD.sql
@@ -0,0 +1,7 @@
+USUÁRIO
+- Nome varchar(30)
+- senha (md5) varchar(60)
+- Email varchar(30)
+
+- Foto de perfil (caminho?, id)
+- Progresso no joguim (código que indica fase e compleção) int
---
+++ Banco de Dados/Insercao_Aquapedia.sql
@@ -0,0 +1,11 @@
+INSERT INTO USUARIOS(USR_EMAIL, USR_SENHA, USR_NOME) VALUES (
+  "alingofilho@gmail.com", "a08502d4b4edd5838f644f27f0970f6e", "aloisinho"
+);
+
+INSERT INTO USUARIOS(USR_EMAIL, USR_SENHA, USR_NOME) VALUES (
+  "clarisserosseti@gmail.com", "e80343df0c3fb0ef5468c8289e5459f8", "kalisse"
+);
+
+INSERT INTO USUARIOS(USR_EMAIL, USR_SENHA, USR_NOME) VALUES (
+  "samuolopes@gmail.com", "64a74c8f05ce61ff55395d8ea467c464", "samuskox"
+);
